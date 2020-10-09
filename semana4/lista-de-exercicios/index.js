8ed02dc578fbe3c11cccd38afe479f7302fe79d0
indiomedeiros <bboyindio@gmail.com>
2020-10-09 12:54:54 -0300

    [Exercicios de interpretação de código] numero 1 e 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -21,3 +21,28 @@ O código é um conversor de moeda, do Dolar para real.
 */
 
 
+/*
+2.
+ O programa calcula o  investimento  e imprime o total do valor acumulado  após o investimento
+ 
+ -existe uma função para calcular o investimento, que recebe dois parâmetros:
+    a - o tipo de investimento;
+    b - o valor investido;
+
+ -dentro da função o parâmetro passado passa por uma condição, que 
+ faz um filtro, comparando com as opções existentes dentro de um Switch case
+
+ - Se o tipo de investimento for diferente das opções fornecida, o programa emite a 
+ alerta no navegador "TIPO DE INVESTIMENTO INFORMADO INCORRETO" e o programa encerra os calculos.
+
+ - se for igual, ele vai gerar um montante, que é o valor referente  parâmetro passado multiplicado
+ pelo valor referente a cada opção que contem no switch case da função;
+    -vai guardar esse valor do montante em uma variável e retorna-la como resultado processado pela função.
+ -enfim, imprime o retorno da função no console do navegador.
+
+ No caso dos valores passados no exercicio os resultados serão:
+  1 - será impresso na primeira linha do console: 165
+  2 - Um alerta será mostrado na tela do navegador com a mensagem ""TIPO DE INVESTIMENTO INFORMADO INCORRETO"
+ */
+
+
