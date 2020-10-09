0ff374e79fac83c838914fbd5c7968b2824d8b45
indiomedeiros <bboyindio@gmail.com>
2020-10-09 12:53:21 -0300

    [Exercicios de interpretação] numero 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,23 @@
+// Exercícios de interpretação de código
+/*
+1.
+O código é um conversor de moeda, do Dolar para real.
+ - o código utiliza uma função para receber do usuário 
+ o valor da cotação do Dólar;
+
+ - A função recebe como parâmetro um valor referente 
+ a quantidade do dolar;
+
+ - o retorno da função é o dólar multiplicado 
+ pela cotação informada pelo usuário; 
+
+ - a função é chamada com a passagem do valor 100 como parâmetro
+ - existe um variável prédefinida que guarda o conteúdo
+  da chamada da função (o resultado )do valor 100 dólares.
+
+ - Ao final é impresso no console o resultado da função, ou seja, se a cotação 
+ do usuário for 2, será impresso o resultado: R$ 200
+
+*/
+
+
