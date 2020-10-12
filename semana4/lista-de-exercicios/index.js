4fffdf9b641ecbb5b85fe27b1310777dbad18a81
indiomedeiros <bboyindio@gmail.com>
2020-10-12 11:03:38 -0300

    [Exercícios de Funções de array] numero 1, 2, 3, 4 e 5

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -336,3 +336,22 @@ const consultasCanceladas = consultas.map((consultas) => {
         contato conosco para remarcá-la`
     }
 })
+
+//5
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+contas.forEach((contas) => {
+    let comprasTotal = 0;
+   for(let i = 0; i < contas.compras.length; i++){
+      comprasTotal += contas.compras[i]  
+   }
+   contas.saldoTotal -= comprasTotal
+})
+console.log (contas)
