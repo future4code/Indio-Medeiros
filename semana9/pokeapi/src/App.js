8409d803cc04207c49ed2f15a3a54a2eda45d10f
Indio <bboyindio@gmail.com>
2020-11-10 18:58:45 -0300

    [exercicios completos] exercicios feitos recriando o documento do zero

--- semana9/pokeapi/src/App.js
+++ semana9/pokeapi/src/App.js
@@ -4,48 +4,50 @@ import axios from "axios";
 import PokeCard from "./components/PokeCard";
 
 
- export default function App () {
-const [pokeList, setPokeList] = useState([])
-const [pokeName, setPokeName] = useState("")
- 
+export default function App () {
+  //state
+  const [pokeList, setPokeList] = useState([])
+  const [pokeName, setPokeName] = useState("")
 
-  // método que roda após a montagem do componente
-  useEffect (() => {
-    // função axios que está batendo na API e buscando 151 pokemons
+  const changePokeName = event => {
+    setPokeName(event.target.value)
+  }
+
+  //pegaLista
+  const getPokeList = () => {
     axios
       .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
-      .then(response => {
-        // função que está setando no estado os 151 pokemons
+      .then((response) => { 
         setPokeList(response.data.results)
+        
       })
-      .catch(err => {
-        console.log(err);
-        });
+      .catch((erro) => {
+        console.log(erro)
+      })
   }
 
-  changePokeName = (event) => {
-    setPokeName(event.target.value)
-  };
+  useEffect(() => {
+    getPokeList(pokeName)
+   
+  },[pokeName])
 
-    return (
-      <div className="App">
-        {/* evento onChange chama função toda vez que o usuário 
-        escolhe um novo pokemon no dropdown */}
-        <select onChange={changePokeName}>
-          <option value={""}>Nenhum</option>
-          {/* renderizando a lista de pokemons como opções do select */}
-          {pokeList.map(pokemon => {
-            return (
-              <option key={pokemon.name} value={pokemon.name}>
-                {pokemon.name}
-              </option>
-            );
-          })}
-        </select>
-        {/* expressão booleana que renderiza o componente PokeCard,
-        caso o valor de pokeName, no estado, seja true */}
-        {pokeName && <PokeCard pokemon={pokeName} />}
-      </div>
-    );
-  }
+
+  return (<div className="App">
+    <select onChange={changePokeName}>
+      <option value={""}>Nenhum</option>
+      {pokeList.map((pokemon) => {
+        return (
+          <option key={pokemon.name} value={pokemon.name}>
+            {pokemon.name}
+          </option>
+        )
+      })}
+    </select>
+
+    {pokeName && <PokeCard pokemon={pokeName}/>}
+    
+  </div> 
+
+  )  
+}
 
