f5fa17657714bde3f6b9dbda0335fb0ab4dcb3e4
indiomedeiros <bboyindio@gmail.com>
2020-12-15 17:13:50 -0300

    [Day1]adicionado até o exercicio 8 + ajustes no inpute ( agora o resultado é imediato, sem botões)

--- semana14/revisao1/src/Componentes/Functions/soluctionDay1.js
+++ semana14/revisao1/src/Componentes/Functions/soluctionDay1.js
@@ -1,54 +1,118 @@
-
-
-
 //exercicios de Funções
 
 //Exercício 1
 export const revertText = (text) => {
-    //reverte o texto
-    return text.split("").reverse().join("")
-}
+  //reverte o texto
+  return text.split("").reverse().join("");
+};
 
 //Exercício 2
 export const checkPalindromo = (text) => {
-    //deixa todo texto minusculo
-    text = text.toLowerCase()
-    //separa, tira os espaços e junta novamente
-    const textWithoutSpace = text.split("").filter((letter) => {return letter !== " "}).join("") 
-    //separa, tira os espaços, inverte e junta novamente
-    const textWithoutSpaceRevert = text.split("").filter((letter) => {return letter !== " "}).reverse().join("")
-    //compara os dois e retorna o resultado
-    if(textWithoutSpace === textWithoutSpaceRevert){
-        return "È um Palindromo"
-    }else{
-        return "Não é um Palindromo"
-    }
-}
+  //deixa todo texto minusculo
+  text = text.toLowerCase();
+  //separa, tira os espaços e junta novamente
+  const textWithoutSpace = text
+    .split("")
+    .filter((letter) => {
+      return letter !== " ";
+    })
+    .join("");
+  //separa, tira os espaços, inverte e junta novamente
+  const textWithoutSpaceRevert = text
+    .split("")
+    .filter((letter) => {
+      return letter !== " ";
+    })
+    .reverse()
+    .join("");
+  //compara os dois e retorna o resultado
+  if (textWithoutSpace === textWithoutSpaceRevert) {
+    return "È um Palindromo";
+  } else {
+    return "Não é um Palindromo";
+  }
+};
 
-export const combinationString = (string) => {
-    
-    let newArrayString = []
-    for(let i = 0; i <= string.length; i++){
-        if (i === 0){
-            newArrayString.push(string[i] + ",")
-        } else if (i < string.length - 1 ) {
-            newArrayString.push(string[i-1] + string[i] + ",")
-        }else if (i === string.length){
-            newArrayString.push(string)
-        }
-    }
-    
-    
-    return newArrayString.join("")
-
-}
 //Exercício 3
+export const combinationString = (string) => {
+  //array de interação
+  let newArrayString = [];
+  for (let i = 0; i < string.length; i++) {
+    //dois contadores (i e j)somando a primeira palavra "i" com as próximas "j"
+    newArrayString.push(string[i] + ",");
+    for (let j = i; j < string.length; j++) {
+      if (string[i] !== string[j]) {
+        newArrayString.push(string[i] + string[j] + ",");
+      }
+      if (j === string.length - 1 && i === 0) {
+        newArrayString.push(string + ",");
+      }
+    }
+  }
+
+  return newArrayString.join("");
+};
 
 //Exercício 4
+export const sortAlphabetic = (string) => {
+  //dividir string, ordenar e considerar os não-ASCII com localeCoompare e juntar tudo
+  return string
+    .split("")
+    .sort(function (a, b) {
+      return a.localeCompare(b);
+    })
+    .join("");
+};
+
 //Exercício 5
+export const addCapitalLetter = (string) => {
+  let formString = "";
+  const arrayNewString = [];
+  const newString = string.toLowerCase().split(" ");
+  for (let i = 0; i < newString.length; i++) {
+    formString = newString[i].split("");
+    if (formString[0]) {
+      formString.splice(0, 1, formString[0].toUpperCase());
+      arrayNewString.push(formString.join(""));
+    }
+  }
+  return arrayNewString.join(" ");
+};
+
 //Exercício 6
+export const checkBiggerWord = (string) => {
+
+    let bigString = ""
+   string.split(" ").forEach((word)=> {
+       // retira o espaço do word inicio e final e compara o tamanho
+         if( word.trim().length > bigString.length) { 
+            bigString = word.trim()
+        }
+    })
+    return bigString
+}
+
 //Exercício 7
+export const countVowels = (string) => {
+    //mapeando vogais
+    const vowels = "áàãâäaéèêëeíìîïióòõôöúùûü"
+    //contador
+    let count = 0
+    string.toLowerCase().split("").forEach((word)=> {
+        //se não houver vogal ele vai retornar -1
+        if( vowels.split("").indexOf(word) !== -1) { 
+          count++
+       }
+   })
+   return count
+}
 //Exercício 8
+export const checkSecondLargestMinor = (numbers) => {
+    let arrayNumbers = []
+    arrayNumbers.push(numbers)
+    let newArrayNumbers = numbers !== " " && arrayNumbers[0].split("").sort()
+    return newArrayNumbers[1] +"," + newArrayNumbers[newArrayNumbers.length -2]
+}
 //Exercício 9
 //Exercício 10
 
--- semana14/revisao1/src/Componentes/Pages/ReviewDay1.js
+++ semana14/revisao1/src/Componentes/Pages/ReviewDay1.js
@@ -1,37 +1,49 @@
 import React, { useState } from "react";
-import {revertText, checkPalindromo, combinationString} from '../Functions/soluctionDay1'
+import {revertText, checkSecondLargestMinor, countVowels, checkPalindromo, checkBiggerWord,  addCapitalLetter, combinationString, sortAlphabetic} from '../Functions/soluctionDay1'
 
 export default function ReviewDay1() {
     const [strings, setStrings] = useState({})
-    const [functionResults, setFunctionResults] = useState({})
     
     const handleOnChange = (event) => {
          const {name, value} = event.target
         setStrings({...strings, [name]: value})
     }
     
-    const handleOnClick = (event) => {
-        const {name, value} = event.target
-        setFunctionResults ({...functionResults, [name]: value})
-        setStrings({...strings, [name]: ""})
-    }
+  
 
   return (
     <div>
         <h3>Reverter texto</h3>
         <input name="revertText" value={strings.revertText} onChange={(handleOnChange)}/>
-        <button name="revertText" onClick={handleOnClick} value={strings.revertText && revertText(strings.revertText)} > Reverter</button> 
-        <p> Resultado: {functionResults.revertText}</p> 
+        <p> Resultado: {strings.revertText && revertText(strings.revertText)}</p> 
         
         <h3>Verificação de Palindromo</h3>
         <input name="checkPalindromo" value={strings.checkPalindromo} onChange={(handleOnChange)}/>
-        <button name="checkPalindromo" onClick={handleOnClick} value={strings.checkPalindromo && checkPalindromo(strings.checkPalindromo)} > Reverter</button> 
-        <p> Resultado: {functionResults.checkPalindromo}</p>
+        <p> Resultado: {strings.checkPalindromo && checkPalindromo(strings.checkPalindromo)}</p>
          
         <h3>Combinações de palavra</h3>
         <input name="combinationString" value={strings.combinationString} onChange={(handleOnChange)}/>
-        <button name="combinationString" onClick={handleOnClick} value={strings.combinationString && combinationString(strings.combinationString)} > Reverter</button> 
-        <p> Resultado: {functionResults.combinationString}</p>
+        <p> Resultado: {strings.combinationString && combinationString(strings.combinationString)}</p>
+
+        <h3>Ordenação alfabetica de palavra</h3>
+        <input name="sortAlphabetic" value={strings.sortAlphabetic} onChange={(handleOnChange)}/>
+        <p> Resultado: {strings.sortAlphabetic && sortAlphabetic(strings.sortAlphabetic)}</p>
+
+        <h3>Adicionar letra maiúscula nas iniciais das palavras</h3>
+        <input name="addCapitalLetter" value={strings.addCapitalLetter} onChange={(handleOnChange)}/>
+        <p> Resultado: {strings.addCapitalLetter && addCapitalLetter(strings.addCapitalLetter)}</p>
+
+        <h3>Mostra maior palavra</h3>
+        <input name="checkBiggerWord" value={strings.checkBiggerWord} onChange={(handleOnChange)}/>
+        <p> Resultado: {strings.checkBiggerWord && checkBiggerWord(strings.checkBiggerWord)}</p>
+
+        <h3>Contar vogais</h3>
+        <input name="countVowels" value={strings.countVowels} onChange={(handleOnChange)}/>
+        <p> Resultado: {strings.countVowels && countVowels(strings.countVowels)} vogais</p>
+
+        <h3>Mostrar o segundo menor e segundo maior número</h3>
+        <input type="number" name="checkSecondLargestMinor" value={strings.checkSecondLargestMinor} onChange={(handleOnChange)}/>
+        <p> Resultado: {strings.checkSecondLargestMinor && checkSecondLargestMinor(strings.checkSecondLargestMinor)} </p>
 
     </div>
 
