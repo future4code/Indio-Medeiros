f7a086556315f5ccc433c4215e11874395fff789
indiomedeiros <bboyindio@gmail.com>
2020-12-16 16:49:06 -0300

    [Day 1] adicionado todas as funções, porém por conta do calculo direto no onChange houve problemas.

--- semana14/revisao1/src/Componentes/Functions/soluctionDay1.js
+++ semana14/revisao1/src/Componentes/Functions/soluctionDay1.js
@@ -81,46 +81,139 @@ export const addCapitalLetter = (string) => {
 
 //Exercício 6
 export const checkBiggerWord = (string) => {
-
-    let bigString = ""
-   string.split(" ").forEach((word)=> {
-       // retira o espaço do word inicio e final e compara o tamanho
-         if( word.trim().length > bigString.length) { 
-            bigString = word.trim()
-        }
-    })
-    return bigString
-}
+  let bigString = "";
+  string.split(" ").forEach((word) => {
+    // retira o espaço do word inicio e final e compara o tamanho
+    if (word.trim().length > bigString.length) {
+      bigString = word.trim();
+    }
+  });
+  return bigString;
+};
 
 //Exercício 7
 export const countVowels = (string) => {
-    //mapeando vogais
-    const vowels = "áàãâäaéèêëeíìîïióòõôöúùûü"
-    //contador
-    let count = 0
-    string.toLowerCase().split("").forEach((word)=> {
-        //se não houver vogal ele vai retornar -1
-        if( vowels.split("").indexOf(word) !== -1) { 
-          count++
-       }
-   })
-   return count
-}
+  //mapeando vogais
+  const vowels = "áàãâäaéèêëeíìîïióòõôöúùûü";
+  //contador
+  let count = 0;
+  string
+    .toLowerCase()
+    .split("")
+    .forEach((word) => {
+      //se não houver vogal ele vai retornar -1
+      if (vowels.split("").indexOf(word) !== -1) {
+        count++;
+      }
+    });
+  return count;
+};
 //Exercício 8
 export const checkSecondLargestMinor = (numbers) => {
-    let arrayNumbers = []
-    arrayNumbers.push(numbers)
-    let newArrayNumbers = numbers !== " " && arrayNumbers[0].split("").sort()
-    return newArrayNumbers[1] +"," + newArrayNumbers[newArrayNumbers.length -2]
-}
-//Exercício 9
-//Exercício 10
+  let arrayNumbers = [];
+  arrayNumbers.push(numbers);
+  let newArrayNumbers = numbers !== " " && arrayNumbers[0].split("").sort();
+  return newArrayNumbers[1] + "," + newArrayNumbers[newArrayNumbers.length - 2] 
+};
 
+//Exercício 9
+export const extractUniqueCharacters = (string) => {
+  //tranformando em array
+  string = string.split("");
+  //novoArray com os caracteres únicos
+  let stringFiltering = [];
+
+  for (let i = 0; i < string.length; i++) {
+    //contador de caracteres repetidos
+    let count = 0;
+
+    for (let j = 0; j < string.length; j++) {
+      //se o caractere for igual a algum dos itens do array
+      //o count vai registrar +1
+      if (string[i] === string[j]) {
+        count++;
+      }
+    }
+    if (count === 1) {
+      stringFiltering.push(string[i]);
+    }
+  }
+
+  return stringFiltering;
+};
+//Exercício 10
+export const getRepeatNumberCaracters = (string) => {
+  string = string.split("");
+  //novoArray com os caracteres únicos
+  let stringFiltering = [];
+
+  for (let i = 0; i < string.length; i++) {
+    //contador de caracteres repetidos
+    let count = 0;
+
+    for (let j = 0; j < string.length; j++) {
+      //se o caractere for igual a algum dos itens do array
+      //o count vai registrar +1
+      if (string[i] === string[j]) {
+        count++;
+      }
+    }
+    if (count > 0 && (string[i] !== " ")) {
+      stringFiltering.push(string[i]+count + ", ");
+    }
+  }
+  //remove caracteres repetidos
+  stringFiltering = stringFiltering.sort().reduce((init, current) => {
+    if (init.length === 0 || init[init.length - 1] !== current) {
+      init.push(current);
+    }
+    return init;
+  }, []);
+
+  return stringFiltering;
+};
 //exercicios de Laços
 
 //Exercício 1
+export const largestNumber = (number) => {
+  let largestNumber = 0;
+  number = number.split(" ")
+ for(let i = 0; i < number.length; i++){
+    if(Number(number[i]) > Number(largestNumber)){
+      largestNumber = number[i]
+    }
+ }
+  return largestNumber
+}
 //Exercício 2
+export const largestWord = (string) => {
+  string = string.split(" ")
+  let largestWord = ""
+  for (let i = 0; i < string.length; i++){
+    if (string[i].length > largestWord.length){
+      largestWord = string[i]
+    }
+  }
+  return largestWord
+}
 //Exercício 3
+export const sortIncreasingNumbers = (number) => {
+  number = number.split("")
+  let auxiliar = ""
+  console.log(number)
+  for(let i = 0; i < number.length ; i++){
+    if(number[i+1] < number[i] ){
+     
+        auxiliar = Number(number[i])
+        number[i] = Number(number[i+1])
+        number[i+1] = Number(auxiliar) 
+
+    }
+}
+      
+  
+  return number
+}
 //Exercício 4
 //Exercício 5
 //Exercício 6
--- semana14/revisao1/src/Componentes/Pages/ReviewDay1.js
+++ semana14/revisao1/src/Componentes/Pages/ReviewDay1.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {revertText, checkSecondLargestMinor, countVowels, checkPalindromo, checkBiggerWord,  addCapitalLetter, combinationString, sortAlphabetic} from '../Functions/soluctionDay1'
+import {revertText,largestWord,sortIncreasingNumbers,  getRepeatNumberCaracters, largestNumber, extractUniqueCharacters,  checkSecondLargestMinor, countVowels, checkPalindromo, checkBiggerWord,  addCapitalLetter, combinationString, sortAlphabetic} from '../Functions/soluctionDay1'
 
 export default function ReviewDay1() {
     const [strings, setStrings] = useState({})
@@ -39,11 +39,32 @@ export default function ReviewDay1() {
 
         <h3>Contar vogais</h3>
         <input name="countVowels" value={strings.countVowels} onChange={(handleOnChange)}/>
-        <p> Resultado: {strings.countVowels && countVowels(strings.countVowels)} vogais</p>
+        <p> Resultado: {strings.countVowels && countVowels(strings.countVowels)} {strings.countVowels && "vogais"}</p>
 
         <h3>Mostrar o segundo menor e segundo maior número</h3>
         <input type="number" name="checkSecondLargestMinor" value={strings.checkSecondLargestMinor} onChange={(handleOnChange)}/>
         <p> Resultado: {strings.checkSecondLargestMinor && checkSecondLargestMinor(strings.checkSecondLargestMinor)} </p>
+
+        <h3>Extrair caracteres únicos de uma String</h3>
+        <input  name="extractUniqueCharacters" value={strings.extractUniqueCharacters } onChange={(handleOnChange)}/>
+        <p> Resultado: {strings.extractUniqueCharacters  && extractUniqueCharacters(strings.extractUniqueCharacters )} </p>
+
+        <h3>Pegar o numero de vezes que cada letra aparece numa string.</h3>
+        <input  name="getRepeatNumberCaracters" value={strings.getRepeatNumberCaracters } onChange={(handleOnChange)}/>
+        <p> Resultado: {strings.getRepeatNumberCaracters  && getRepeatNumberCaracters(strings.getRepeatNumberCaracters)} </p>
+
+        <h1>LAÇOS</h1>
+        <h3>Pegar o maior numero</h3>
+        <input  name="largestNumber" value={strings.largestNumber } onChange={(handleOnChange)}/>
+        <p> Resultado: {strings.largestNumber && largestNumber(strings.largestNumber)} </p>
+
+        <h3>Pegar a maior palavra</h3>
+        <input name="largestWord" value={strings.largestWord } onChange={(handleOnChange)}/>
+        <p> Resultado: {strings.largestWord && largestWord(strings.largestWord)} </p>
+
+        <h3>Ordenação crescente de numeros (até 3 numeros)</h3>
+        <input  maxLength="3" name="sortIncreasingNumbers" value={strings.largestWord } onChange={(handleOnChange)}/>
+        <p> Resultado: {strings.sortIncreasingNumbers && sortIncreasingNumbers(strings.sortIncreasingNumbers)} </p>
 
     </div>
 
