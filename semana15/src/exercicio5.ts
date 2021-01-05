5c35261e3cc7f6af42c2bb2fd3331cbc7176d63a
Indio Medeiros <bboyindio@gmail.com>
2021-01-05 16:11:23 -0300

    Desafio: exercicio 5

---
+++ semana15/src/exercicio5.ts
@@ -0,0 +1,13 @@
+function somar (numero1: number, numero2: number) {
+    console.log("entrada:", numero1, "e", numero2)
+    console.log ("a) Soma:", numero1 + numero2);
+    console.log("b) Subtração:", numero1 - numero2);
+    console.log("c) multiplicação:", numero1 * numero2);
+     
+    if(numero1 > numero2){
+        console.log("d) maior:", numero1)
+    }else{
+        console.log("d) maior:", numero2)
+    }
+}
+somar(10, 30)
