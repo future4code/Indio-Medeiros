128e75305634fd3acf60ce71e470fec6bd68ffe9
Indio Medeiros <bboyindio@gmail.com>
2021-01-05 15:28:56 -0300

    exercicio 2

---
+++ semana15/src/exercicio2.ts
@@ -0,0 +1,31 @@
+//a e b
+function obterEstatisticas(numeros: number[])  {
+
+    const numerosOrdenados: number[] = numeros.sort(
+        (a: number, b: number) => a - b
+    )
+
+    let soma: number = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas: {maior: number, menor: number, media: number} = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
+
+//c)
+type amostraDeIdades = {
+    numeros: number[]
+    obterEstatisticas: (numeros: number[]) => {
+        maior: number,
+        menor: number,
+        media: number
+    }
+}
