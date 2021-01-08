0657b0471f08342fa2d8313a358641a3f0433bdc
Indio Medeiros <bboyindio@gmail.com>
2021-01-08 16:42:07 -0300

    Adicionado os requisitos minímos

--- semana15/Projeto/src/index.ts
+++ semana15/Projeto/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
-import {userAccount, userExtract, users} from "./database"
+import { userAccount, userExtract, users } from "./database";
 
 const app = express();
 app.use(express.json);
@@ -9,31 +9,62 @@ app.use(cors());
 
 //cadastrar usuário
 app.post("/users/register", (req: Request, res: Response) => {
-    let errorCode: number = 400
+  let errorCode: number = 400;
 
-    try{
-
-    if(req.body.birth.year < 2003){
-        throw new Error ("Só é possível cadastrar pessoas com ou acimade 18 anos.")
+  try {
+    if (req.body.birth.year < 2003) {
+      throw new Error(
+        "Só é possível cadastrar pessoas com ou acimade 18 anos."
+      );
     }
 
-    }catch(error){
-        res.status(errorCode).send(error.message)
+    if (
+      req.body.name === undefined ||
+      req.body.CPF === undefined ||
+      req.body.birth.day === undefined ||
+      req.body.birth.moth === undefined
+    ) {
+      throw new Error("Algo deu errado com sua requisição (verificar o body).");
     }
-})
+
+    const user: userAccount = {
+      name: req.body.name,
+      CPF: req.body.CPF,
+      birth: {
+        day: req.body.birth.day,
+        moth: req.body.birth.moth,
+        year: req.body.birth.year,
+      },
+      balance: 0,
+      transactions: ["Não possui transações"],
+    };
+    users.push(user);
+
+    res.status(200).send("Conta criada com sucesso! Bem vindo ao F4Bank");
+  } catch (error) {
+    res.status(errorCode).send(error.message);
+  }
+});
 
 //pegar todos usuários existentes
 app.get("/users/all", (req: Request, res: Response) => {
+  let errorCode: number = 400;
 
-})
-
-
+  try {
+    if (!users || Number(users) === 0) {
+      res.status(200).send("Ainda não existe usuários");
+    }
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(errorCode).send("error.message");
+  }
+});
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Server is running in http://localhost: ${address.port}`);
   } else {
-    console.error("Failure upon starting server");
+    console.error(`Failure upon starting server.`);
   }
 });
