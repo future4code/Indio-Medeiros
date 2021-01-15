e9dc83c5a8827084df30b551d79d594d3c05f9b2
indiomedeiros <bboyindio@gmail.com>
2021-01-15 16:02:56 -0300

    Foi criado o endipoint PUT:criação do usutário + validação

--- semana16/Projeto/src/index.ts
+++ semana16/Projeto/src/index.ts
@@ -22,7 +22,45 @@ export const connection = knex({
   },
 });
 
-
+//criar usuário
+app.put("/user", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    let errorMessage: string = `(verifique o body) ausência da propriedade  `
+  
+    try {
+      //validação-------
+      if (!req.body.name || req.body.name === undefined) {
+          errorCode = 404
+          throw new Error(errorMessage + "*name")
+      }
+  
+      if (!req.body.nickname || req.body.nickname === undefined ) {
+          errorCode = 404
+          throw new Error(errorMessage + "*nickname")
+      }
+  
+      if (!req.body.email ||req.body.email === undefined) {
+          errorCode = 404
+          throw new Error(errorMessage + "*email")    
+      }
+      //------------------
+      
+      //criação de usuário
+      const userCreate: user = {
+          id: Date.now(),
+          name: req.body.name,
+          nickname: req.body.nickname,
+          email: req.body.email
+      }
+      
+      users.push(userCreate)
+  
+      res.status(200).send('usuário criado com sucesso!')
+  
+    } catch (error) {
+        res.status(errorCode).send(error.message)
+    }
+  });
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
