ab2c0e8f3640a12d59fe0dcbfa11a531eddc7bb3
indiomedeiros <bboyindio@gmail.com>
2021-02-02 14:44:45 -0300

    exercicio 2

---
+++ semana20/aula55/src/data/model/task.ts
@@ -0,0 +1,10 @@
+export type createTaskInputDTO = {
+         title: string ,
+         description: string,
+         deadline: string,
+         authorId: string
+}
+
+export type getTaskInputDTO = {
+    id: string
+}
---
+++ semana20/aula55/src/data/model/user.ts
@@ -0,0 +1,12 @@
+export type signupInputDTO = {
+    name: string,
+    nickname: string,
+    password: string,
+    email: string,
+    role: string
+}
+
+export type loginInputDTO = {
+    email: string,
+    password: string
+}
