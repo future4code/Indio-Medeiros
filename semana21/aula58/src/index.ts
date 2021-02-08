3fdefef22e3539e38e69544061745b4a039d3b18
indiomedeiros <bboyindio@gmail.com>
2021-02-08 16:17:14 -0300

    [Testes automatizados com Typescript] - Exercicio 4

--- semana21/aula58/src/index.ts
+++ semana21/aula58/src/index.ts
@@ -13,3 +13,4 @@ export const performPurchase = (
   }
   return undefined;
 };
+
--- semana21/aula58/tests/index.test.ts
+++ semana21/aula58/tests/index.test.ts
@@ -1,5 +1,7 @@
 import { performPurchase } from "../src";
-import { User } from "../src/entities/user";
+import { LOCATION, NACIONALITY } from "../src/entities/enums";
+import { Casino, User, UserNacionatily } from "../src/entities/user";
+import { checkAge } from "../src/services/checks";
 
 //2a.
 test("Testing balance greater than value", () => {
@@ -41,4 +43,87 @@ test("Testing balance greater than value", () => {
   const result = performPurchase(user, 50);
 
   expect(result).toEqual(undefined);
+});
+
+//4a.
+test("1 Testing brasilizan allowed", () => {
+  const brasilizan: UserNacionatily = {
+    name: "indio",
+    age: 19,
+    nacionality: NACIONALITY.BRAZILIAN,
+  };
+
+  const casino: Casino = {
+    name: "Discoteca baiana",
+    location: LOCATION.BRAZIL,
+  };
+
+  const result = checkAge(casino, [brasilizan]);
+  expect(result.brazilians.allowed).toEqual(["indio"]);
+});
+
+//4b.
+test("1  american allowed", () => {
+  const brasilizan: UserNacionatily = {
+    name: "indio",
+    age: 19,
+    nacionality: NACIONALITY.AMERICAN,
+  };
+
+  const casino: Casino = {
+    name: "Discoteca baiana",
+    location: LOCATION.BRAZIL,
+  };
+
+  const result = checkAge(casino, [brasilizan]);
+  expect(result.americans.allowed).toEqual(["indio"]);
+});
+
+//4c.
+test("1 No one allowed", () => {
+  const brasilizan: UserNacionatily = {
+    name: "indio",
+    age: 19,
+    nacionality: NACIONALITY.BRAZILIAN,
+  };
+
+  const american: UserNacionatily = {
+    name: "indian",
+    age: 19,
+    nacionality: NACIONALITY.AMERICAN,
+  };
+
+  const casino: Casino = {
+    name: "Discoteca baiana",
+    location: LOCATION.EUA,
+  };
+
+  const result = checkAge(casino, [brasilizan, brasilizan, american, american]);
+
+  expect(result.brazilians.unallowed).toEqual(["indio", "indio"]);
+  expect(result.americans.unallowed).toEqual(["indian", "indian"]);
+});
+
+test("2 american allowed and 2 brazilian unallowed", () => {
+  const brasilizan: UserNacionatily = {
+    name: "indio BR",
+    age: 19,
+    nacionality: NACIONALITY.BRAZILIAN,
+  };
+
+  const american: UserNacionatily = {
+    name: "indio EUA",
+    age: 21,
+    nacionality: NACIONALITY.AMERICAN,
+  };
+
+  const casino: Casino = {
+    name: "Discoteca americana",
+    location: LOCATION.EUA,
+  };
+
+  const result = checkAge(casino, [brasilizan, brasilizan, american, american]);
+
+  expect(result.brazilians.unallowed).toEqual(["indio BR", "indio BR"]);
+    expect(result.americans.allowed).toEqual(["indio EUA", "indio EUA"]);
 });
