a4772fa363f7235403e8b733eefb6e1f9715cfa3
indiomedeiros <bboyindio@gmail.com>
2021-02-09 14:06:08 -0300

    [Testes unitários e Mocks] - exercicio 1

---
+++ semana21/aula59/src/entities/character.ts
@@ -0,0 +1,8 @@
+
+//1a.
+export interface Character {
+    name: string,
+    life: number, 
+    strength: number,
+    defense: number,
+}
---
+++ semana21/aula59/src/services/checkCharacter.ts
@@ -0,0 +1,23 @@
+import { Character } from "../entities/character"
+
+//2b.
+export const validateCharacter = (input: Character): boolean => {
+    if(
+        !input.name ||
+        input.life === undefined ||
+        input.strength === undefined ||
+        input.defense === undefined
+    ){
+        return false
+    }
+
+    if(
+        input.life <= 0 || 
+        input.strength <= 0 ||
+        input.defense <= 0
+    ){
+        return false
+    }
+
+    return true
+}
